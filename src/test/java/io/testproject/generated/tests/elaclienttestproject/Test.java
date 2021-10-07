c0c82110c8b0ef0d3ad9993ce325697dfc4cf072
sajidali <sajidali@192.168.200.251>
2021-10-07 21:35:46 +0530

    First Commit
    Uploading all the test cases

---
+++ src/test/java/io/testproject/generated/tests/elaclienttestproject/Test.java
@@ -0,0 +1,9 @@
+package io.testproject.generated.tests.elaclienttestproject;
+
+import io.appium.java_client.MobileElement;
+import io.testproject.sdk.drivers.android.AndroidDriver;
+import java.lang.Exception;
+
+interface Test {
+  void execute(AndroidDriver<? extends MobileElement> driver) throws Exception;
+}
